5e9cf70e41319c3cec13723f7224afc219e328c9
eggle <eggleston.pan@outlook.com>
2020-10-16 19:22:04 +0800

    +关键词count

--- oasis/src/main/java/com/example/oasisdemo/blImpl/AuthorDisplay/AuthorDisplayServiceImpl.java
+++ oasis/src/main/java/com/example/oasisdemo/blImpl/AuthorDisplay/AuthorDisplayServiceImpl.java
@@ -196,8 +196,38 @@ public class AuthorDisplayServiceImpl implements  AuthorDisplayService{
     public ResponseVO getAuthorTopicsById(String id) {
         try {
             String str=authorDisplayMapper.selectAuthorTopicsById(id);
-            List<String> res=Arrays.asList(str.split("\n|;"));
-            return ResponseVO.buildSuccess(res);
+            List<String> aff=Arrays.asList(str.split("\n|;"));
+            HashMap<String,Integer> res=new HashMap<>();
+            for(int i=0;i<aff.size();i++){
+                Set<String> wordSet=res.keySet();
+                String topic=aff.get(i);
+                //如果已经有这个单词了，
+                if(topic.equals("\r")){
+                    continue;
+                }
+                if(wordSet.contains(topic))
+                {
+                    Integer number=res.get(topic);
+                    number++;
+                    res.put(topic, number);
+                }
+                else {
+                    res.put(topic, 1);
+                }
+            }
+            List<HashMap.Entry<String, Integer>> list = new ArrayList<HashMap.Entry<String, Integer>>(res.entrySet());
+            Collections.sort(list, new Comparator<HashMap.Entry<String, Integer>>() {
+                //降序排序
+                @Override
+                public int compare(HashMap.Entry<String, Integer> o1, HashMap.Entry<String, Integer> o2) {
+                    return o2.getValue().compareTo(o1.getValue());
+                }
+            });
+//            StringBuilder resStr=new StringBuilder("{");
+//            for (HashMap.Entry<String, Integer> mapping : list) {
+//                resStr.append(mapping.getKey()).append(",").append(mapping.getValue()).append(";");
+//            }
+            return ResponseVO.buildSuccess(list);
         } catch (Exception e){
             e.printStackTrace();
             return ResponseVO.buildFailure("失败");
